1c698d524fe56806b99a52f1531a4e8e0d544e12
HarleyLounsbury <harley.lounsbury@gmail.com>
2020-10-14 11:36:17 -0400

    Validations succursale

--- src/models/succursale.js
+++ src/models/succursale.js
@@ -8,23 +8,41 @@ import mongoose from 'mongoose';
 
 const succursaleSchema = mongoose.Schema({
     appelatif: { type: String, unique: true },
-    adresse: String,
-    ville: String,
-    codePostal: String,
-    province: String,
-    telephone: String,
-    telecopieur: String,
-    information: String
-    
+    adresse: { type: String, maxlength: 255, required: true },
+    ville: { type: String, maxlength: 255, required: true },
+    codePostal: {
+        type: String, validate: {
+            validator: function (v) {
+                return /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/.test(v);
+            }
+        }
+    },
+    province: { type: String, maxlength: 50, required: true },
+    telephone: {
+        type: String, maxlength: 15, required: true, validate: {
+            validator: function (v) {
+                return /\d{3}-\d{3}-\d{4}/.test(v);
+            }
+        }
+    },
+    telecopieur: {
+        type: String, maxlength: 15, required: true, validate: {
+            validator: function (v) {
+                return /\d{3}-\d{3}-\d{4}/.test(v);
+            }
+        }
+    },
+    information: { type: String, maxlength: 255 }
+
 }, {
     collection: 'succursales'
 });
 
-succursaleSchema.virtual('inventaires',{
-    ref:'Inventaire',
-    localField:'_id',
-    foreignField:'succursale',
-    justOne:false
+succursaleSchema.virtual('inventaires', {
+    ref: 'Inventaire',
+    localField: '_id',
+    foreignField: 'succursale',
+    justOne: false
 });
 
 export default mongoose.model('Succursale', succursaleSchema);
